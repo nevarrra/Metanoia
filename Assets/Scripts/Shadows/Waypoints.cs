1e199e32cfb4afc3b631ba6dde5f331b8d10f686
Gabriel Vergari <gabrielvergari@hotmail.com>
2021-04-27 16:28:05 +0100

    New map + waypoints + navmesh

---
+++ Assets/Scripts/Shadows/Waypoints.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public Waypoints[] edges;
+
+    private void OnDrawGizmos()
+    {
+        if (edges != null)
+        {
+            foreach (Waypoints waypoints in edges)
+            {
+                Gizmos.DrawLine(transform.position, waypoints.transform.position);
+            }
+        }
+    }
+}
