5c8bb80106194fbe9aefc7cd97f53f74fdab23cb
Maria <kandushk@gmail.com>
2021-05-16 23:27:20 +0100

    very small search fix
    shadows will now turn to look at the player when he escapes to the light

--- Assets/Scripts/AI/Actions/Shadows/SearchAction.cs
+++ Assets/Scripts/AI/Actions/Shadows/SearchAction.cs
@@ -8,6 +8,7 @@ public class SearchAction : Action
     public override void Act(FSM entity)
     {
         entity.GetAgent().SetDestinationTo(entity.GetAgent().imaginaryFriend.LastPlayerPosition);
+        entity.GetAgent().transform.LookAt(entity.GetAgent().player.transform.position);
         entity.GetAgent().imaginaryFriend.InitialSearchTimer -= Time.deltaTime;
 
     }
