f34fca4b17342d91c326ea1b1440a3aa7eab8938
Gabriel Vergari <gabrielvergari@hotmail.com>
2021-05-20 12:51:03 +0100

    s

--- Assets/Scripts/ImaginaryFriends/IFAttributes.cs
+++ Assets/Scripts/ImaginaryFriends/IFAttributes.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public class IFAttributes : Attributes
 {
-    public override float Speed { get; set; } = 1000000000f;
-    public override float VisionRange { get; set; } = 7f;
+    public override float Speed { get; set; } = 3.5f;
+    public override float VisionRange { get; set; } = 12f;
 }
